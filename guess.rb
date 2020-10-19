8ce222b6cba05b479a368904ad19ec870eb48c14
Jem Pimentel <jem.pimentel@outlook.com>
2020-10-19 17:21:50 +0800

    Added number of tries

--- guess.rb
+++ guess.rb
@@ -3,12 +3,17 @@
 number = rand(1..100)
 puts "I'm thinking of a number from 1 to 100."
 
+acc = 0
+
 loop do
   print "Guess my number! "
   guess = gets.to_i
+  # acc = acc + 1
+  acc += 1
 
   if guess == number
     puts "You're correct! My number is #{number}."
+    puts "Number of guesses: #{acc}"
     break
   elsif guess < number
     puts "Higher!"
