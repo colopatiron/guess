c1ab8529648824b5695d498e1cfdceaf5a66c62c
Jem Pimentel <jem.pimentel@outlook.com>
2020-10-19 17:01:12 +0800

    Added looping

--- guess.rb
+++ guess.rb
@@ -3,13 +3,15 @@
 number = rand(1..100)
 puts "I'm thinking of a number from 1 to 100."
 
-print "Guess my number! "
-guess = gets.to_i
+loop do
+  print "Guess my number! "
+  guess = gets.to_i
 
-if guess == number
-  puts "You're correct! My number is #{number}."
-elsif guess < number
-  puts "Higher!"
-else
-  puts "Lower!"
+  if guess == number
+    puts "You're correct! My number is #{number}."
+  elsif guess < number
+    puts "Higher!"
+  else
+    puts "Lower!"
+  end
 end
