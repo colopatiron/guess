e7ea4cfac2321cfe6221a61cdadec13fed4a4e04
Jem Pimentel <jem.pimentel@outlook.com>
2020-10-19 16:49:25 +0800

    Started work on guess.rb

---
+++ guess.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+number = rand(1..100)
+puts "I'm thinking of a number from 1 to 100."
+
+print "Guess my number! "
+guess = gets.to_i
+
+if guess == number
+  puts "You're correct! My number is #{number}."
+elsif guess < number
+  puts "Higher!"
+else
+  puts "Lower!"
+end
